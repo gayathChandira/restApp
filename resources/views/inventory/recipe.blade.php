10eee9ce748e6848b3429937a76ff3be1817ae45
Gayath <gayath.chandira94@gmail.com>
2018-10-03 19:47:30 +0530

    modify recipe

--- resources/views/inventory/recipe.blade.php
+++ resources/views/inventory/recipe.blade.php
@@ -2,23 +2,24 @@
 
 @section('content')
     <h1>Make a New Recipe</h1>
-    {!! Form::open(['action'=>'RecipeController@store', 'method' =>'POST'])!!}
+    {!! Form::open(['action'=>'RecipeController@store', 'method' =>'POST','autocomplete' =>'off'])!!}
         <div class="form-group">
             {{Form::label('recipeName', 'Meal Name')}}
             {{Form::text('recipeName', '', ['class' =>'form-control', 'placeholder'=>'Recipe', 'id'=>'item_ID'])}}
             <!-- {{-- <input type="text" class="form-control" id="item_ID"> --}} -->
             <div id="item_list" style="z-index: 1;position:absolute;"></div>            
         </div>
+        {{ csrf_field() }}  
         <div id="room_fileds">
             <div class="form-row">
                 <div class="col form-group"  >
                     {{Form::label('ingredients', 'Ingredients')}}
-                    {{Form::text('array1[0][0]', '', ['class' =>'form-control', 'placeholder'=>'Item'])}}
-                                        
+                    {{Form::text('ingri[0]', '', ['class' =>'form-control item_name', 'placeholder'=>'Item'])}}
+                    <div id="name_list" style="z-index: 1;position:absolute;"></div>                     
                 </div> 
                 <div class="col form-group ">
                     {{Form::label('amounts', 'Amounts')}}
-                    {{Form::text('array1[0][1]', '', ['class' =>'form-control', 'placeholder'=>'Amounts'])}}
+                    {{Form::text('amount[0]', '', ['class' =>'form-control', 'placeholder'=>'Amounts'])}}
                 </div> 
             </div>  
         </div>      
@@ -32,18 +33,46 @@
     {!! Form::close() !!}
      
     <script>
+        
+        $(document).ready(function(){
+            //Get ingridient name when use type the name in text box
+            $('.item_name').keyup(function(){
+                var query = $(this).val(); 
+                console.log(query);                           
+                if(query != ''){                    
+                    var _token = $('input[name="_token"]').val();
+                    $.ajax({
+                        url:"{{ route('FoodItemController.fetchNameWhenType')}}",    
+                        method:"POST",
+                        data:{query:query, _token:_token},
+                        success:function(data){                            
+                            $('#name_list').fadeIn();
+                            $('#name_list').html(data);
+                        }           
+                     })
+                }
+            });
+            //set item-id when user selcts the item name from the dropdown list
+            if($('.item_name').keyup()){ 
+                $(document).on('click', '#list2', function(){                
+                $('#item_name').val($(this).text());
+                $('#name_list').fadeOut();  
+
+                });
+            }
+
+        });
         var room = 0;      
         function add_fields() {
             room++;
             var objTo = document.getElementById('room_fileds')
             var divtest = document.createElement("div");
-            divtest.innerHTML = '<div class="form-row"><div class="form-group col"><label>Ingrediants</label><input type ="text" placeholder="Item" name="array1['+room+'][0]" class="form-control"></div><div class="form-group col"><label>Amounts</label><input type ="text" placeholder="Amounts" name="array1['+room+'][1]" class="form-control"></div></div>';
+            divtest.innerHTML = '<div class="form-row"><div class="form-group col"><label>Ingrediants</label><input type ="text" placeholder="Item" name="ingri['+room+']" class="form-control item_name"><div id="name_list" style="z-index: 1;position:absolute;"></div>   </div><div class="form-group col"><label>Amounts</label><input type ="text" placeholder="Amounts" name="amount['+room+']" class="form-control"></div></div>';
             objTo.appendChild(divtest);
-            document.getElementById('length').value = room;
+            document.getElementById('length').value = room+1;
             var hello = document.getElementById("length").value
             //console.log(hello);
-        }    
-      
+        } 
     </script>
     
 @endsection
